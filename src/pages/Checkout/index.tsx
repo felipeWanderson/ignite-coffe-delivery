8d6d950276debf63aee72dcf3666e911b81879bb
Felipe Leal <felipe.leal@compasso.com.br>
2022-12-17 01:25:37 -0300

    feat: checkout - address form

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,9 +1,45 @@
-import { CheckoutContainer } from './styles'
+import { MapPinLine } from 'phosphor-react'
+import {
+  AddressContainer,
+  AddressHeader,
+  CheckoutContainer,
+  InfoOrderContainer,
+  ResumeOrder,
+  Title,
+  Icon,
+  Info,
+  InfoTitle,
+  InfoSubTitle,
+  FormAddress,
+} from './styles'
 
 export function Checkout() {
   return (
     <CheckoutContainer>
-      <h1>Checkout</h1>
+      <InfoOrderContainer>
+        <Title>Complete seu pedido</Title>
+        <AddressContainer>
+          <AddressHeader>
+            <Icon>
+              <MapPinLine size={22} />
+            </Icon>
+            <Info>
+              <InfoTitle>Endereço de Entrega</InfoTitle>
+              <InfoSubTitle>
+                Informe o endereço onde deseja receber seu pedido
+              </InfoSubTitle>
+            </Info>
+          </AddressHeader>
+
+          <FormAddress>
+            <input type="text" />
+          </FormAddress>
+        </AddressContainer>
+      </InfoOrderContainer>
+
+      <ResumeOrder>
+        <Title>Cafés selecionados</Title>
+      </ResumeOrder>
     </CheckoutContainer>
   )
 }
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -2,4 +2,85 @@ import styled from 'styled-components'
 
 export const CheckoutContainer = styled.main`
   padding: 4rem 16rem;
+
+  display: flex;
+  justify-content: space-between;
+
+  gap: 32px;
 `
+
+export const InfoOrderContainer = styled.section`
+  max-width: 640px;
+  width: 100%;
+  height: 100%;
+`
+
+export const AddressContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 40px;
+  gap: 32px;
+
+  background: ${(props) => props.theme['gray-100']};
+  border-radius: 6px;
+`
+export const AddressHeader = styled.div`
+  width: 100;
+  display: flex;
+  align-items: flex-start;
+
+  gap: 8px;
+`
+export const Icon = styled.div`
+  svg {
+    line {
+      stroke: ${(props) => props.theme['yellow-700']};
+    }
+    circle {
+      stroke: ${(props) => props.theme['yellow-700']};
+    }
+    path {
+      stroke: ${(props) => props.theme['yellow-700']};
+    }
+  }
+`
+export const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 2px;
+`
+
+export const InfoText = styled.span`
+  font-family: 'Roboto', sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 130%;
+  color: ${(props) => props.theme['gray-800']};
+`
+export const InfoTitle = styled(InfoText)`
+  font-size: 16px;
+`
+export const InfoSubTitle = styled(InfoText)`
+  font-size: 14px;
+`
+export const ResumeOrder = styled.section`
+  border: 1px solid #000;
+
+  max-width: 448px;
+  width: 100%;
+  height: 100%;
+`
+
+export const Title = styled.h3`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 130%;
+
+  color: ${(props) => props.theme['gray-800']};
+  margin-bottom: 16px;
+`
+
+export const FormAddress = styled.form``
